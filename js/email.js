8e6c1c2745cda227826f7c354079aff022e10acb
osancus <omer.shafiq2@gmail.com>
2023-08-13 03:16:25 +0300

    leadfeeder integration

--- js/email.js
+++ js/email.js
@@ -1,6 +1,7 @@
 function newsletterEmail() {
     emailjs.init('WOOJsVTLL2TnSIie3');
     window.onload = function () {
+        (function (ss, ex) { window.ldfdr = window.ldfdr || function () { (ldfdr._q = ldfdr._q || []).push([].slice.call(arguments)); }; (function (d, s) { fs = d.getElementsByTagName(s)[0]; function ce(src) { var cs = d.createElement(s); cs.src = src; cs.async = 1; fs.parentNode.insertBefore(cs, fs); }; ce('https://sc.lfeeder.com/lftracker_v1_' + ss + (ex ? '_' + ex : '') + '.js'); })(document, 'script'); })('Xbp1oaEoDeX7EdVj');
         document.getElementById('newsletter-form').addEventListener('submit', function (event) {
             event.preventDefault();
 
--- js/header-and-footer.js
+++ js/header-and-footer.js
@@ -1,7 +1,6 @@
 class Header extends HTMLElement {
   connectedCallback() {
-    this.innerHTML = `      
-    <script> (function(ss,ex){ window.ldfdr=window.ldfdr||function(){(ldfdr._q=ldfdr._q||[]).push([].slice.call(arguments));}; (function(d,s){ fs=d.getElementsByTagName(s)[0]; function ce(src){ var cs=d.createElement(s); cs.src=src; cs.async=1; fs.parentNode.insertBefore(cs,fs); }; ce('https://sc.lfeeder.com/lftracker_v1_'+ss+(ex?'_'+ex:'')+'.js'); })(document,'script'); })('Xbp1oaEoDeX7EdVj'); </script>
+    this.innerHTML = `          
         <header class="theme-main-menu sticky-menu theme-menu-one">
         <div class="inner-content">
             <div class="d-flex align-items-center justify-content-between">
